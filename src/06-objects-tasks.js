eae9a80839be6f2a2c907eb5212752a1898d31e9
Maksim Perevyazkin <kvadratpm@gmail.com>
2020-12-13 13:07:14 +0300

    feat: complete object-tasks

--- src/06-objects-tasks.js
+++ src/06-objects-tasks.js
@@ -20,8 +20,13 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+
+  this.getArea = function getArea() {
+    return this.width * this.height;
+  };
 }
 
 
@@ -35,8 +40,8 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
@@ -51,8 +56,8 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  return new proto.constructor(...Object.values(JSON.parse(json)));
 }
 
 
@@ -111,35 +116,61 @@ function fromJSON(/* proto, json */) {
  */
 
 const cssSelectorBuilder = {
-  element(/* value */) {
-    throw new Error('Not implemented');
+  exElement: '',
+  exId: '',
+  exClass: '',
+  exAttr: '',
+  exPseudoClass: '',
+  exPseudoElement: '',
+  exCombine: '',
+
+
+  element(value) {
+    this.exElement += value;
+    return this;
   },
 
-  id(/* value */) {
-    throw new Error('Not implemented');
+  id(value) {
+    this.exId += `#${value}`;
+    return this;
   },
 
-  class(/* value */) {
-    throw new Error('Not implemented');
+  class(value) {
+    this.exClass += `.${value}`;
+    return this;
   },
 
-  attr(/* value */) {
-    throw new Error('Not implemented');
+  attr(value) {
+    this.exAttr += `[${value}]`;
+    return this;
   },
 
-  pseudoClass(/* value */) {
-    throw new Error('Not implemented');
+  pseudoClass(value) {
+    this.exPseudoClass += `:${value}`;
+    return this;
   },
 
-  pseudoElement(/* value */) {
-    throw new Error('Not implemented');
+  pseudoElement(value) {
+    this.exPseudoElement += `::${value}`;
+    return this;
   },
 
   combine(/* selector1, combinator, selector2 */) {
     throw new Error('Not implemented');
   },
-};
 
+  stringify() {
+    const string = `${this.exElement}${this.exId}${this.exClass}${this.exAttr}${this.exPseudoClass}${this.exPseudoElement}${this.exCombine}`;
+    this.exElement = '';
+    this.exId = '';
+    this.exClass = '';
+    this.exAttr = '';
+    this.exPseudoClass = '';
+    this.exPseudoElement = '';
+    this.exCombine = '';
+    return string;
+  },
+};
 
 module.exports = {
   Rectangle,
