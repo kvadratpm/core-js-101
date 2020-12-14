4d7ddc6e7d5343f4328ebc761f6449d1b48ee747
Maksim Perevyazkin <kvadratpm@gmail.com>
2020-12-14 19:21:38 +0300

    feat: complete some function tasks

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -23,8 +23,8 @@
  *   getComposition(Math.sin, Math.asin)(x) => Math.sin(Math.asin(x))
  *
  */
-function getComposition(/* f, g */) {
-  throw new Error('Not implemented');
+function getComposition(f, g) {
+  return (x) => f(g(x));
 }
 
 
@@ -44,8 +44,8 @@ function getComposition(/* f, g */) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (n) => n ** exponent;
 }
 
 
@@ -81,8 +81,16 @@ function getPolynom() {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  const log = {};
+  return (newFunc) => {
+    if (newFunc in log) {
+      return log[newFunc];
+    }
+    const result = func();
+    log[newFunc] = result;
+    return result;
+  };
 }
 
 
@@ -169,7 +177,7 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
+function getIdGeneratorFunction(/* id */) {
   throw new Error('Not implemented');
 }
 
